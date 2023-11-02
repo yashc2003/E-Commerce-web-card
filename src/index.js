356831b02b8a214312cce2e92f5d713b4168470b
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2023-11-02 10:41:14 +0000

    update file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import Card from './Components/Cards/Card';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Card/>
-);
-
